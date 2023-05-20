a176886736f0ef94185a17aceb335eac5b7e9506
Correcaminoscastell <134145752+Correcaminoscastell@users.noreply.github.com>
2023-05-20 16:25:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,22 @@ const menu = document.querySelector('.menu');
 menuIcon.addEventListener('click', function() {
   menu.classList.toggle('show');
 });
+document.addEventListener('DOMContentLoaded', function() {
+  var menuLinks = document.querySelectorAll('.menu a');
+
+  menuLinks.forEach(function(link) {
+    link.addEventListener('click', function(e) {
+      e.preventDefault();
+      var targetId = this.getAttribute('href');
+      var target = document.querySelector(targetId);
+
+      if (target) {
+        var offsetTop = target.offsetTop;
+        window.scrollTo({
+          top: offsetTop,
+          behavior: 'smooth'
+        });
+      }
+    });
+  });
+});
